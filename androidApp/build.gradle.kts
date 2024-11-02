536853599fa13f79ef803ecf3abf05022b22c477
hagiharatomoaki <j8110074@gmail.com>
2024-11-02 23:11:32 +0900

    [Update] Koinの追加

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -42,5 +42,7 @@ dependencies {
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
     debugImplementation(libs.compose.ui.tooling)
 }
