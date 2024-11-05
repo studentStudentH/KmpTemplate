560b890c678479db13111034be14e22d3c0aae6a
hagiharatomoaki <j8110074@gmail.com>
2024-11-06 02:46:09 +0900

    [Add] MainApplicationを追加

---
+++ androidApp/src/main/java/com/example/kmptemplate/android/MainApplication.kt
@@ -0,0 +1,18 @@
+package com.example.kmptemplate.android
+
+import android.app.Application
+import com.example.kmptemplate.shareDiModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication(): Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(shareDiModule, appDiModule)
+        }
+    }
+}
