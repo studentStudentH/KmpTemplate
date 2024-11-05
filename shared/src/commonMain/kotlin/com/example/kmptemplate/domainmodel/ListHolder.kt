3114a0f43af535bd463c9661c1c7e64cf4a0bfdf
hagiharatomoaki <j8110074@gmail.com>
2024-11-06 02:44:15 +0900

    [Add] ListHolderを追加

---
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/ListHolder.kt
@@ -0,0 +1,9 @@
+package com.example.kmptemplate.domainmodel
+
+/**
+ * Skieの制約で
+ * - GenericsはAnyに準拠する
+ * - Generic<List<A>>の代わりにGeneric<ListHolder<A>>とする
+ * もしそうしないとSwiftに変換した時、List内部の型が失われてしまう
+ */
+data class ListHolder<T: Any>(val list: List<T>)
