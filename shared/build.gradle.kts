672bbaa96f0b6ef65f8e1b9b44cf7d826c54558b
hagiharatomoaki <j8110074@gmail.com>
2024-11-07 00:56:39 +0900

    [Update] sharedモジュールにcoroutineCoreへの依存関係を追加

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -34,6 +34,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             // put your multiplatform dependencies here
+            implementation(libs.kotlinx.coroutines.core)
             implementation(project.dependencies.platform(libs.koin.bom))
             implementation(libs.koin.core)
         }
