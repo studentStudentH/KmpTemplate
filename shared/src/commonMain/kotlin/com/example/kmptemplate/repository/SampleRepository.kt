ee47bae1a9ea3ad3cc9a2d5a462232f899af771a
hagiharatomoaki <j8110074@gmail.com>
2024-11-07 00:59:33 +0900

    [Update] SampleRepositoryのシグネチャをiOSでも型情報付きで取得できる形式に変更

--- shared/src/commonMain/kotlin/com/example/kmptemplate/repository/SampleRepository.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/repository/SampleRepository.kt
@@ -1,21 +1,29 @@
 package com.example.kmptemplate.repository
 
+import com.example.kmptemplate.domainmodel.KmpResult
 import com.example.kmptemplate.domainmodel.ListHolder
+import kotlinx.coroutines.delay
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 interface SampleRepository {
-    suspend fun getSampleSentences(): Result<ListHolder<String>>
+    suspend fun getSampleWords(): KmpResult<ListHolder<String>>
 }
 
 internal class DevSampleRepositoryImpl: SampleRepository {
-    override suspend fun getSampleSentences(): Result<ListHolder<String>> {
+    override suspend fun getSampleWords(): KmpResult<ListHolder<String>> {
+        // 通信遅延の表現
+        delay(5.0.toDuration(DurationUnit.SECONDS))
         val tempList = listOf("Dev A", "Dev B", "Dev C")
-        return Result.success(ListHolder(tempList))
+        return KmpResult.Success(ListHolder(tempList))
     }
 }
 
 internal class ReleaseSampleRepositoryImpl: SampleRepository {
-    override suspend fun getSampleSentences(): Result<ListHolder<String>> {
+    override suspend fun getSampleWords(): KmpResult<ListHolder<String>> {
+        // 通信遅延の表現
+        delay(5.0.toDuration(DurationUnit.SECONDS))
         val tempList = listOf("Release A", "Release B", "Release C")
-        return Result.success(ListHolder(tempList))
+        return KmpResult.Success(ListHolder(tempList))
     }
 }
