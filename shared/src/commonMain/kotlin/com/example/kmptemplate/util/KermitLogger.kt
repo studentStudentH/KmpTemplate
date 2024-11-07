209a6719aee3e5ef7cd88da642ad9fabfe4380f4
hagiharatomoaki <j8110074@gmail.com>
2024-11-07 23:13:44 +0900

    [Clean] ktFormat実行

--- shared/src/commonMain/kotlin/com/example/kmptemplate/util/KermitLogger.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/util/KermitLogger.kt
@@ -3,19 +3,31 @@ package com.example.kmptemplate.util
 import co.touchlab.kermit.Logger
 
 object KermitLogger {
-    fun d(tag: String, message: () -> String) {
+    fun d(
+        tag: String,
+        message: () -> String,
+    ) {
         Logger.d(tag) { message() }
     }
 
-    fun i(tag: String, message: () -> String) {
+    fun i(
+        tag: String,
+        message: () -> String,
+    ) {
         Logger.i(tag) { message() }
     }
 
-    fun w(tag: String, message: () -> String) {
+    fun w(
+        tag: String,
+        message: () -> String,
+    ) {
         Logger.w(tag) { message() }
     }
 
-    fun e(tag: String, message: () -> String) {
+    fun e(
+        tag: String,
+        message: () -> String,
+    ) {
         Logger.e(tag) { message() }
     }
 }
