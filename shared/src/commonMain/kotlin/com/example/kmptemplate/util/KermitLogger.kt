9261375b3bd8bb8021e3fb5e8052469f5c1bd43f
hagiharatomoaki <j8110074@gmail.com>
2024-11-07 23:10:46 +0900

    [Add] KermitLoggerを追加

---
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/util/KermitLogger.kt
@@ -0,0 +1,21 @@
+package com.example.kmptemplate.util
+
+import co.touchlab.kermit.Logger
+
+object KermitLogger {
+    fun d(tag: String, message: () -> String) {
+        Logger.d(tag) { message() }
+    }
+
+    fun i(tag: String, message: () -> String) {
+        Logger.i(tag) { message() }
+    }
+
+    fun w(tag: String, message: () -> String) {
+        Logger.w(tag) { message() }
+    }
+
+    fun e(tag: String, message: () -> String) {
+        Logger.e(tag) { message() }
+    }
+}
