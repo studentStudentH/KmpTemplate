b7ecf83fa8402bc1b0b1a34c980033b7466e3779
hagiharatomoaki <j8110074@gmail.com>
2024-11-09 22:05:37 +0900

    [Add] YearMonthを追加

---
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/YearMonth.kt
@@ -0,0 +1,11 @@
+package com.example.kmptemplate.domainmodel
+
+data class YearMonth @Throws(IllegalArgumentException::class) constructor(
+    val year: Int,
+    val month: Int
+) {
+    init {
+        if (year < 0) throw IllegalArgumentException("yearは0以上にしてください")
+        if (month < 1 || month > 12) throw IllegalArgumentException("monthは1以上12以下にしてください")
+    }
+}
