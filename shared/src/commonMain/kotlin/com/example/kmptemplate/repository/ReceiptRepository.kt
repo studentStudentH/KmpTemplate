ee8f62718d32e5596429fca7ff417ca6ab69624e
hagiharatomoaki <j8110074@gmail.com>
2024-11-10 00:28:37 +0900

    [Add] ReceiptRepositoryを追加

---
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/repository/ReceiptRepository.kt
@@ -0,0 +1,38 @@
+package com.example.kmptemplate.repository
+
+import com.example.kmptemplate.domainmodel.KmpResult
+import com.example.kmptemplate.domainmodel.Receipt
+import com.example.kmptemplate.domainmodel.ReceiptCollection
+import kotlinx.datetime.Instant
+
+interface ReceiptRepository {
+    suspend fun getReceiptsNewerThan(
+        year: Int,
+        month: Int
+    ): KmpResult<ReceiptCollection>
+
+    suspend fun getReceiptsBetween(
+        oldestYear: Int,
+        oldestMonth: Int,
+        newestYear: Int,
+        newestMonth: Int
+    ): KmpResult<ReceiptCollection>
+
+    /**
+     * 返り値は新規作成したReceiptが入る
+     */
+    suspend fun add(
+        cost: Int,
+        category: String,
+        createdAt: Instant,
+    ): KmpResult<Receipt>
+
+    suspend fun update(
+        id: Int,
+        cost: Int,
+        category: String,
+        createdAt: Instant
+    ): KmpResult<Receipt>
+
+    suspend fun delete(id: Int): KmpResult<Receipt>
+}
