a223fddc21cea7aac9c7077f3e2a278c4a0d4ae5
hagiharatomoaki <j8110074@gmail.com>
2024-11-09 22:18:08 +0900

    [Update] Receiptに主キーとなるidを追加

--- shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/Receipt.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/Receipt.kt
@@ -3,6 +3,7 @@ package com.example.kmptemplate.domainmodel
 import kotlinx.datetime.Instant
 
 /**
+ * @param id: データのid。自動瀬性するのでクライアントで指定する必要は無い想定
  * @param cost: 何円かかったか
  * @param category: 何に支払ったか
  * @param createdAt: いつに支払ったか
@@ -10,6 +11,7 @@ import kotlinx.datetime.Instant
 data class Receipt
     @Throws(IllegalArgumentException::class)
     constructor(
+        val id: Int = 0,
         val cost: Int,
         val category: String,
         val createdAt: Instant,
