c4e0ed7878d564ed1cfa7916627ac3be9f3b5459
hagiharatomoaki <j8110074@gmail.com>
2024-11-10 17:11:35 +0900

    [Update] FeeCategoryにRoomのアノテーションを追加

--- shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/FeeCategory.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/FeeCategory.kt
@@ -1,12 +1,15 @@
 package com.example.kmptemplate.domainmodel
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.datetime.Instant
 
 /**
  * @param name: カテゴリ名
  * @param lastUsedAt: 最後に使われた日時
  */
+@Entity
 data class FeeCategory(
-    val name: String,
+    @PrimaryKey(autoGenerate = false) val name: String,
     val lastUsedAt: Instant,
 )
