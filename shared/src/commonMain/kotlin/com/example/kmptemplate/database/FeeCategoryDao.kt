5ec4adf586f55ec3a01eaafef1ccac5f14515f0b
hagiharatomoaki <j8110074@gmail.com>
2024-11-10 17:13:51 +0900

    [Add] FeeCategoryDaoを追加

---
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/database/FeeCategoryDao.kt
@@ -0,0 +1,24 @@
+package com.example.kmptemplate.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.kmptemplate.domainmodel.FeeCategory
+
+@Dao
+interface FeeCategoryDao {
+    @Query("SELECT * FROM FeeCategory")
+    suspend fun getAll(): List<FeeCategory>
+
+    @Insert(onConflict =OnConflictStrategy.IGNORE)
+    suspend fun insert(vararg feeCategories: FeeCategory)
+
+    @Update
+    suspend fun update(vararg feeCategories: FeeCategory)
+
+    @Delete
+    suspend fun delete(vararg feeCategories: FeeCategory)
+}
