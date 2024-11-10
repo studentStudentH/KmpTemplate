1b96a5cc7d83ad4527ec464a9835b680bfb7374a
hagiharatomoaki <j8110074@gmail.com>
2024-11-10 17:15:04 +0900

    [Add] 両OSごとにAppDatabaseを提供するdiModuleを追加

---
+++ shared/src/androidMain/kotlin/com/example/kmptemplate/PlatformModule.kt
@@ -0,0 +1,26 @@
+package com.example.kmptemplate
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.kmptemplate.database.AppDatabase
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+fun makePlatformModule(application: Application): Module {
+    return module {
+        single<AppDatabase> {
+            getDatabaseBuilder(application).build()
+        }
+    }
+}
+
+private fun getDatabaseBuilder(ctx: Context): RoomDatabase.Builder<AppDatabase> {
+    val appContext = ctx.applicationContext
+    val dbFile = appContext.getDatabasePath("my_room.db")
+    return Room.databaseBuilder<AppDatabase>(
+        context = appContext,
+        name = dbFile.absolutePath
+    )
+}
---
+++ shared/src/iosMain/kotlin/com/example/kmptemplate/PlatformModule.kt
@@ -0,0 +1,36 @@
+package com.example.kmptemplate
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.kmptemplate.database.AppDatabase
+import kotlinx.cinterop.ExperimentalForeignApi
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
+
+internal val platformModule: Module = module {
+    single<AppDatabase> {
+        getDatabaseBuilder().build()
+    }
+}
+
+fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
+    val dbFilePath = documentDirectory() + "/my_room.db"
+    return Room.databaseBuilder<AppDatabase>(
+        name = dbFilePath,
+    )
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun documentDirectory(): String {
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null,
+    )
+    return requireNotNull(documentDirectory?.path)
+}
