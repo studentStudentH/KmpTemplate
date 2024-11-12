07921196b9b91b73bb7f60137ff01dff7a1e615b
hagiharatomoaki <j8110074@gmail.com>
2024-11-13 00:49:44 +0900

    [Update] FeeCategoryRepositoryを内部でcacheを持たない想定のIFに変更

--- shared/src/commonMain/kotlin/com/example/kmptemplate/repository/FeeCategoryRepository.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/repository/FeeCategoryRepository.kt
@@ -1,31 +1,35 @@
 package com.example.kmptemplate.repository
 
+import com.example.kmptemplate.domainmodel.FeeCategory
 import com.example.kmptemplate.domainmodel.FeeCategoryCollection
 import com.example.kmptemplate.domainmodel.KmpResult
 
 interface FeeCategoryRepository {
     suspend fun getAllCategory(): KmpResult<FeeCategoryCollection>
 
+
     /**
-     * 返り値は最新のすべてのFeeCategoryの一覧
+     * 返り値は追加されたカテゴリ
      */
-    suspend fun addCategory(name: String): KmpResult<FeeCategoryCollection>
+    suspend fun addCategory(name: String): KmpResult<FeeCategory>
 
     /**
      * あるカテゴリについて、そのカテゴリを使ったReceiptを新規作成したり、
      * 既存のReceiptに割り当てたりした時に呼び出す
      * そのカテゴリの最終利用時刻を現在の時刻に設定する
-     * 返り値は最新のすべてのFeeCategoryの一覧ß
+     * 返り値は追加されたカテゴリ
      */
-    suspend fun updateLastUsedTime(name: String): KmpResult<FeeCategoryCollection>
+    suspend fun updateLastUsedTime(categoryId: Int): KmpResult<FeeCategory>
 
     /**
-     * 既存のカテゴリのカテゴリ名を修正する。oldNameが登録されていなかった場合はエラーß
+     * 既存のカテゴリのカテゴリ名を修正する。oldNameが登録されていなかった場合はエラー
+     * 返り値は更新後のカテゴリ
      */
-    suspend fun renameCategory(oldName: String, newName: String): KmpResult<FeeCategoryCollection>
+    suspend fun renameCategory(categoryId: Int, newName: String): KmpResult<FeeCategory>
 
     /**
      * 既存のカテゴリを削除する。そのカテゴリが登録されていなかった場合はエラー
+     * 返り値は成否のみ
      */
-    suspend fun deleteCategory(name: String): KmpResult<FeeCategoryCollection>
+    suspend fun deleteCategory(categoryId: Int): KmpResult<Unit>
 }
