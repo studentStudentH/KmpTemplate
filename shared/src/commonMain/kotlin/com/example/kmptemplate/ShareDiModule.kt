16b49a2bf43500f0a95ddffeca23a0a2f24c12e7
hagiharatomoaki <j8110074@gmail.com>
2024-11-13 00:53:10 +0900

    [Update] RepositoryやDataSourceのDI設定を追加

--- shared/src/commonMain/kotlin/com/example/kmptemplate/ShareDiModule.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/ShareDiModule.kt
@@ -1,6 +1,11 @@
 package com.example.kmptemplate
 
+import com.example.kmptemplate.database.AppDatabase
+import com.example.kmptemplate.datasource.FeeCategoryDataSource
+import com.example.kmptemplate.datasource.RoomFeeCategoryDataSource
 import com.example.kmptemplate.repository.DevSampleRepositoryImpl
+import com.example.kmptemplate.repository.FeeCategoryRepository
+import com.example.kmptemplate.repository.FeeCategoryRepositoryImpl
 import com.example.kmptemplate.repository.ReleaseSampleRepositoryImpl
 import com.example.kmptemplate.repository.SampleRepository
 import org.koin.core.module.Module
@@ -14,6 +19,13 @@ val shareDiModule: Module =
             } else {
                 DevSampleRepositoryImpl()
             }
+        }
+        single<FeeCategoryDataSource> {
+            val dao = get<AppDatabase>().getFeeCategoryDao()
+            RoomFeeCategoryDataSource(dao)
+        }
+        single<FeeCategoryRepository> {
+            FeeCategoryRepositoryImpl(get())
         }
     }
 
