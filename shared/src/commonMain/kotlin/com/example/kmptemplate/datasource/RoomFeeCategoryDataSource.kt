7db960ec5a05963258dc973419beabd41a2cbbea
hagiharatomoaki <j8110074@gmail.com>
2024-11-13 00:47:45 +0900

    [Update] IFの変更に合わせてRoomFeeCategoryDataSourceを更新

--- shared/src/commonMain/kotlin/com/example/kmptemplate/datasource/RoomFeeCategoryDataSource.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/datasource/RoomFeeCategoryDataSource.kt
@@ -4,6 +4,10 @@ import com.example.kmptemplate.database.FeeCategoryDao
 import com.example.kmptemplate.domainmodel.FeeCategory
 import com.example.kmptemplate.domainmodel.KmpError
 import com.example.kmptemplate.domainmodel.KmpResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
+import kotlinx.datetime.Clock
 
 /**
  * ToDo: エラーハンドリングが適当すぎるのでちゃんとする
@@ -12,38 +16,68 @@ class RoomFeeCategoryDataSource(
     private val feeCategoryDao: FeeCategoryDao
 ): FeeCategoryDataSource  {
     override suspend fun getAllCategory(): KmpResult<List<FeeCategory>> {
-        try {
-            val list = feeCategoryDao.loadAll()
-            return KmpResult.Success(list)
-        } catch(e: Exception) {
-            return KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーです"))
+        return withContext(Dispatchers.IO) {
+            try {
+                val list = feeCategoryDao.loadAll()
+                KmpResult.Success(list)
+            } catch(e: Exception) {
+                KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーです"))
+            }
         }
     }
 
-    override suspend fun addCategory(vararg feeCategories: FeeCategory): KmpResult<Unit> {
-        try {
-            feeCategoryDao.insert(*feeCategories)
-            return KmpResult.Success(Unit)
-        } catch (e: Exception) {
-            return KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーですß"))
+    override suspend fun addCategory(
+        feeCategoryInputs: List<FeeCategoryInput>,
+    ): KmpResult<List<FeeCategory>> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val convertedItems = feeCategoryInputs.map {
+                    FeeCategory(name = it.name, lastUsedAt = it.lastUsedAt)
+                }
+                feeCategoryDao.insert(convertedItems)
+                val queryNames = feeCategoryInputs.map { it.name }
+                val addedItems = feeCategoryDao.loadByNames(queryNames)
+                KmpResult.Success(addedItems)
+            } catch (e: Exception) {
+                KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーです"))
+            }
         }
     }
 
-    override suspend fun update(vararg feeCategories: FeeCategory): KmpResult<Unit> {
-        try {
-            feeCategoryDao.update(*feeCategories)
-            return KmpResult.Success(Unit)
-        } catch (e: Exception) {
-            return KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーですß"))
+    override suspend fun updateLastUsedTime(categoryId: Int): KmpResult<FeeCategory> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val data = feeCategoryDao.loadById(categoryId)
+                val updatedData = data.copy(lastUsedAt = Clock.System.now())
+                feeCategoryDao.update(listOf(updatedData))
+                KmpResult.Success(updatedData)
+            } catch (e: Exception) {
+                KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーです"))
+            }
         }
     }
 
-    override suspend fun delete(vararg feeCategories: FeeCategory): KmpResult<Unit> {
-        try {
-            feeCategoryDao.delete(*feeCategories)
-            return KmpResult.Success(Unit)
-        } catch (e: Exception) {
-            return KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーですß"))
+    override suspend fun renameCategory(categoryId: Int, newName: String): KmpResult<FeeCategory> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val data = feeCategoryDao.loadById(categoryId)
+                val updatedData = data.copy(name = newName, lastUsedAt = Clock.System.now())
+                feeCategoryDao.update(listOf(updatedData))
+                KmpResult.Success(updatedData)
+            } catch (e: Exception) {
+                KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーです"))
+            }
+        }
+    }
+
+    override suspend fun delete(feeCategories: List<FeeCategory>): KmpResult<Unit> {
+        return withContext(Dispatchers.IO) {
+            try {
+                feeCategoryDao.delete(feeCategories)
+                KmpResult.Success(Unit)
+            } catch (e: Exception) {
+                KmpResult.Failure(KmpError.ServerError(e.message ?: "不明なエラーです"))
+            }
         }
     }
 }
