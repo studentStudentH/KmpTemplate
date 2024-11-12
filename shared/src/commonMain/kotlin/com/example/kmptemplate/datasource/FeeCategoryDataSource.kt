f45725bf8f289188460cf25e17630a7b1f901e96
hagiharatomoaki <j8110074@gmail.com>
2024-11-13 00:46:53 +0900

    [Update] FeeCategoryDataSourceのvarargをリストに変更 + update系メソッドを修正

--- shared/src/commonMain/kotlin/com/example/kmptemplate/datasource/FeeCategoryDataSource.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/datasource/FeeCategoryDataSource.kt
@@ -6,9 +6,21 @@ import com.example.kmptemplate.domainmodel.KmpResult
 interface FeeCategoryDataSource {
     suspend fun getAllCategory(): KmpResult<List<FeeCategory>>
 
-    suspend fun addCategory(vararg feeCategories: FeeCategory): KmpResult<Unit>
+    suspend fun addCategory(feeCategoryInputs: List<FeeCategoryInput>): KmpResult<List<FeeCategory>>
 
-    suspend fun update(vararg feeCategories: FeeCategory): KmpResult<Unit>
+    /**
+     * あるカテゴリについて、そのカテゴリを使ったReceiptを新規作成したり、
+     * 既存のReceiptに割り当てたりした時に呼び出す
+     * そのカテゴリの最終利用時刻を現在の時刻に設定する
+     * 返り値は追加されたカテゴリ
+     */
+    suspend fun updateLastUsedTime(categoryId: Int): KmpResult<FeeCategory>
 
-    suspend fun delete(vararg feeCategories: FeeCategory): KmpResult<Unit>
+    /**
+     * 既存のカテゴリのカテゴリ名を修正する。oldNameが登録されていなかった場合はエラー
+     * 返り値は更新後のカテゴリ
+     */
+    suspend fun renameCategory(categoryId: Int, newName: String): KmpResult<FeeCategory>
+
+    suspend fun delete(feeCategories: List<FeeCategory>): KmpResult<Unit>
 }
