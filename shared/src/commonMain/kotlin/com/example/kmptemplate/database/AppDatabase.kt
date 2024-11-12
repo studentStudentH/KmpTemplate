0c1e03b8df36ba056e6bd8952a8a09fddcdf163a
hagiharatomoaki <j8110074@gmail.com>
2024-11-13 00:50:35 +0900

    [Update] AppDatabaseにTypeConverterの設定を追加

--- shared/src/commonMain/kotlin/com/example/kmptemplate/database/AppDatabase.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/database/AppDatabase.kt
@@ -4,10 +4,14 @@ import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
+import androidx.room.TypeConverters
 import com.example.kmptemplate.domainmodel.FeeCategory
 
 @Database(entities = [FeeCategory::class], version = 1)
 @ConstructedBy(AppDatabaseConstructor::class)
+@TypeConverters(
+    InstantConverter::class,
+)
 abstract class AppDatabase : RoomDatabase(){
     abstract fun getFeeCategoryDao(): FeeCategoryDao
 }
