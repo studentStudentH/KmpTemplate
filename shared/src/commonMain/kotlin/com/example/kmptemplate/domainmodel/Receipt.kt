7298781caa856dacb2a8387794021b7d9b35dfe0
hagiharatomoaki <j8110074@gmail.com>
2024-11-14 00:19:20 +0900

    [Update] Receiptのカテゴリをnull許容型に変更

--- shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/Receipt.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/Receipt.kt
@@ -5,7 +5,7 @@ import kotlinx.datetime.Instant
 /**
  * @param id: データのid。自動瀬性するのでクライアントで指定する必要は無い想定
  * @param cost: 何円かかったか
- * @param category: 何に支払ったか
+ * @param category: 何に支払ったか。nullは値が設定されていないことを意味する
  * @param createdAt: いつに支払ったか
  */
 data class Receipt
@@ -13,7 +13,7 @@ data class Receipt
     constructor(
         val id: Int = 0,
         val cost: Int,
-        val category: FeeCategory,
+        val category: FeeCategory?,
         val createdAt: Instant,
     ) {
         init {
