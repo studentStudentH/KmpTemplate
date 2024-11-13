41519d2040188642248a70a4899012cfca7a0aab
hagiharatomoaki <j8110074@gmail.com>
2024-11-13 22:41:49 +0900

    [Update] iOS用のDiContainerからFeeCategoryRepositoryを取得できるように変更

--- shared/src/iosMain/kotlin/com/example/kmptemplate/DiContainer.kt
+++ shared/src/iosMain/kotlin/com/example/kmptemplate/DiContainer.kt
@@ -1,9 +1,11 @@
 package com.example.kmptemplate
 
+import com.example.kmptemplate.repository.FeeCategoryRepository
 import com.example.kmptemplate.repository.SampleRepository
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 object DiContainer : KoinComponent {
     val sampleRepository: SampleRepository by inject()
+    val feeCategoryRepository: FeeCategoryRepository by inject()
 }
