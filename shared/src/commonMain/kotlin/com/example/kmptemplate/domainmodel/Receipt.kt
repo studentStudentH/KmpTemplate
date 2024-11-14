a0a43a7531d82a1f8411d5999fa76d9891868946
hagiharatomoaki <j8110074@gmail.com>
2024-11-14 22:05:02 +0900

    [Update] ReceiptのidをStringに変更

--- shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/Receipt.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/domainmodel/Receipt.kt
@@ -3,7 +3,7 @@ package com.example.kmptemplate.domainmodel
 import kotlinx.datetime.Instant
 
 /**
- * @param id: データのid。自動瀬性するのでクライアントで指定する必要は無い想定
+ * @param id: データのid
  * @param cost: 何円かかったか
  * @param category: 何に支払ったか。nullは値が設定されていないことを意味する
  * @param createdAt: いつに支払ったか
@@ -11,7 +11,7 @@ import kotlinx.datetime.Instant
 data class Receipt
     @Throws(IllegalArgumentException::class)
     constructor(
-        val id: Int = 0,
+        val id: String,
         val cost: Int,
         val category: FeeCategory?,
         val createdAt: Instant,
