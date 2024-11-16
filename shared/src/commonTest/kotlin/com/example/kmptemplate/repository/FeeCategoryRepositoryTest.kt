a769d652c25082a3293a3dfbbadf25ae31b42347
hagiharatomoaki <j8110074@gmail.com>
2024-11-16 16:34:38 +0900

    [Add] FeeCategoryRepositoryのすべてのテストを実装

--- shared/src/commonTest/kotlin/com/example/kmptemplate/repository/FeeCategoryRepositoryTest.kt
+++ shared/src/commonTest/kotlin/com/example/kmptemplate/repository/FeeCategoryRepositoryTest.kt
@@ -50,4 +50,109 @@ class FeeCategoryRepositoryTest {
             }
         }
     }
+
+    @Test
+    fun testAddCategory() = runTest(
+        timeout = 5.0.toDuration(DurationUnit.SECONDS)
+    ) {
+        // given
+        val dataHolder = SimulatedDataHolder.makeTestData()
+        val dao = SimulatedFeeCategoryDao(dataHolder)
+        val datasource = RoomFeeCategoryDataSource(dao)
+        val repository = FeeCategoryRepositoryImpl(datasource)
+
+        // when
+        val result = repository.addCategory("test")
+
+        // then
+        when (result) {
+            is KmpResult.Failure -> {
+                fail("addCategory()が失敗しています。error =  ${result.error}")
+            }
+            is KmpResult.Success -> {
+                val actual = result.value
+                assertEquals(
+                    "追加したデータがresultで帰ってきていません",
+                    "test",
+                    actual.name
+                )
+                assertEquals(
+                    "保存されたはずなのにデータ件数が期待値と一致しません",
+                    5,
+                    dataHolder.feeCategoryList.size
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testRenameCategory() = runTest(
+        timeout = 5.0.toDuration(DurationUnit.SECONDS)
+    ) {
+        // given
+        val dataHolder = SimulatedDataHolder.makeTestData()
+        val dao = SimulatedFeeCategoryDao(dataHolder)
+        val datasource = RoomFeeCategoryDataSource(dao)
+        val repository = FeeCategoryRepositoryImpl(datasource)
+        val targetCategory = dataHolder.feeCategoryList.first()
+
+        // when
+        val newCategoryName = "rewrote"
+        val result = repository.renameCategory(targetCategory.id, newCategoryName)
+        val rewroteCategory = dataHolder.feeCategoryList.first { it.id == targetCategory.id }
+
+        // then
+        when (result) {
+            is KmpResult.Failure -> {
+                fail("renameCategory()が失敗しています。error =  ${result.error}")
+            }
+            is KmpResult.Success -> {
+                val actual = result.value
+                assertEquals(
+                    "適切な編集結果がresultで帰ってきていません",
+                    expected = newCategoryName,
+                    actual = actual.name
+                )
+                assertEquals(
+                    "上書きされたはずのデータが保存されていません",
+                    expected = newCategoryName,
+                    actual = rewroteCategory.name
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testDeleteCategory() = runTest(
+        timeout = 5.0.toDuration(DurationUnit.SECONDS)
+    ) {
+        // given
+        val dataHolder = SimulatedDataHolder.makeTestData()
+        val dao = SimulatedFeeCategoryDao(dataHolder)
+        val datasource = RoomFeeCategoryDataSource(dao)
+        val repository = FeeCategoryRepositoryImpl(datasource)
+        val targetCategory = dataHolder.feeCategoryList.first()
+
+        // when
+        val result = repository.deleteCategory(targetCategory.id)
+
+        // then
+        when (result) {
+            is KmpResult.Failure -> {
+                fail("deleteCategory()が失敗しています。error =  ${result.error}")
+            }
+            is KmpResult.Success -> {
+                assertEquals(
+                    "削除したはずなのにデータ件数が減っていません",
+                    expected = 3,
+                    actual = dataHolder.feeCategoryList.size
+                )
+                val toBeNull = dataHolder.feeCategoryList.find { it.id == targetCategory.id }
+                if (toBeNull != null) {
+                    fail("削除したはずのデータがなぜか見つかります")
+                }
+            }
+        }
+
+    }
 }
