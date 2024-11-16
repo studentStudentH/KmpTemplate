58ad7d5d567b069fd0943d5c409350d895870bc3
hagiharatomoaki <j8110074@gmail.com>
2024-11-17 02:55:01 +0900

    [Update] get()系メソッドのテストケースを実装

--- shared/src/commonTest/kotlin/com/example/kmptemplate/repository/ReceiptRepositoryTest.kt
+++ shared/src/commonTest/kotlin/com/example/kmptemplate/repository/ReceiptRepositoryTest.kt
@@ -1,17 +1,24 @@
 package com.example.kmptemplate.repository
 
+import com.example.kmptemplate.database.RoomReceipt
 import com.example.kmptemplate.database.SimulatedDataHolder
 import com.example.kmptemplate.database.SimulatedFeeCategoryDao
 import com.example.kmptemplate.database.SimulatedRoomReceiptDao
 import com.example.kmptemplate.datasource.RoomFeeCategoryDataSource
 import com.example.kmptemplate.datasource.RoomReceiptDataSource
+import com.example.kmptemplate.domainmodel.FeeCategory
 import com.example.kmptemplate.domainmodel.KmpResult
+import com.example.kmptemplate.domainmodel.YearMonth
 import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
 import kotlin.test.DefaultAsserter.assertEquals
 import kotlin.test.Test
 import kotlin.test.fail
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 class ReceiptRepositoryTest {
     @Test
@@ -42,14 +49,67 @@ class ReceiptRepositoryTest {
     fun testGetReceiptsNewerThan() = runTest(
         timeout = 5.0.toDuration(DurationUnit.SECONDS),
     ) {
-        // ToDo
+        // given
+        val dataHolder = makeDataHolderForFilterMethod()
+        val repository = makeRepository(dataHolder)
+
+        // when
+        val result = repository.getReceiptsNewerThan(2024, 10)
+
+        // then
+        checkResult(result, "getReceiptsNewerThan()が失敗しています。") { receiptCollection ->
+            val receipts = receiptCollection.sortByInstantDecending()
+            assertEquals(
+                "Receiptの件数が期待と違います",
+                expected = 2,
+                actual = receipts.size
+            )
+            val yearMonth = receiptCollection.getPrevYearMonth()
+            assertEquals(
+                "取得したReceiptの前の月は9月のはずです",
+                expected = 9,
+                actual = yearMonth.month,
+            )
+        }
     }
 
     @Test
     fun testGetReceiptsBetween() = runTest(
         timeout = 5.0.toDuration(DurationUnit.SECONDS),
     ) {
-        // ToDo
+        // given
+        val dataHolder = makeDataHolderForFilterMethod()
+        val repository = makeRepository(dataHolder)
+
+        // when
+        val result = repository.getReceiptsBetween(
+            oldestYear = 2024,
+            oldestMonth = 9,
+            newestYear = 2024,
+            newestMonth = 10,
+        )
+
+        // then
+        checkResult(result, "getReceiptsNewerThan()が失敗しています。") { receiptCollection ->
+            val receipts = receiptCollection.sortByInstantDecending()
+            assertEquals(
+                "Receiptの件数が期待と違います",
+                expected = 2,
+                actual = receipts.size
+            )
+            val prevYearMonth = receiptCollection.getPrevYearMonth()
+            assertEquals(
+                "取得したReceiptの前の月は8月のはずです",
+                expected = 8,
+                actual = prevYearMonth.month
+            )
+            val nextYearMonth = receiptCollection.getNextYearMonth()
+            assertEquals(
+                "取得したReceiptの次の月は11月のはずです",
+                expected = 11,
+                actual = nextYearMonth.month
+            )
+        }
     }
 
     @Test
@@ -71,6 +131,43 @@ class ReceiptRepositoryTest {
         timeout = 5.0.toDuration(DurationUnit.SECONDS),
     ) {
         // ToDo
+    }
+
+    /**
+     * 11/1, 10/1, 9/01, 08/01が作成日のReceiptを持つ
+     * SimulatedDataHolderを作成する
+     */
+    @OptIn(ExperimentalUuidApi::class)
+    private fun makeDataHolderForFilterMethod(): SimulatedDataHolder {
+        val year = 2024
+        val baseMonth = 11
+        val feeCategories =
+            FeeCategory.INITIAL_CATEGORIES.map { categoryName ->
+                val id = Uuid.random().toHexString()
+                // カテゴリの作成日は適当な値を入れる
+                val yearMonth = YearMonth(year, baseMonth)
+                FeeCategory(
+                    id,
+                    categoryName,
+                    yearMonth.toLocalDateTime().toInstant(TimeZone.currentSystemDefault())
+                )
+            }
+        val roomReceipts =
+            feeCategories.mapIndexed { index, feeCategory ->
+                val id = Uuid.random().toHexString()
+                val cost = 1000
+                val yearMonth = YearMonth(year, baseMonth - index)
+                RoomReceipt(
+                    id,
+                    cost,
+                    feeCategory.id,
+                    yearMonth.toLocalDateTime().toInstant(TimeZone.currentSystemDefault())
+                )
+            }
+        return SimulatedDataHolder(
+            feeCategories,
+            roomReceipts,
+        )
     }
 
     private fun makeRepository(
