7b95823114ec71a4a52fc308285e54eb66af963d
hagiharatomoaki <j8110074@gmail.com>
2024-11-18 01:02:31 +0900

    [Update] sharedのflavorに応じて利用するDAOを変更できるように実装

--- shared/src/commonMain/kotlin/com/example/kmptemplate/ShareDiModule.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/ShareDiModule.kt
@@ -3,11 +3,20 @@ package com.example.kmptemplate
 import androidx.room.RoomDatabase
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.example.kmptemplate.database.AppDatabase
+import com.example.kmptemplate.database.FeeCategoryDao
+import com.example.kmptemplate.database.RoomReceiptDao
+import com.example.kmptemplate.database.SimulatedDataHolder
+import com.example.kmptemplate.database.SimulatedFeeCategoryDao
+import com.example.kmptemplate.database.SimulatedRoomReceiptDao
 import com.example.kmptemplate.datasource.FeeCategoryDataSource
+import com.example.kmptemplate.datasource.ReceiptDataSource
 import com.example.kmptemplate.datasource.RoomFeeCategoryDataSource
+import com.example.kmptemplate.datasource.RoomReceiptDataSource
 import com.example.kmptemplate.repository.DevSampleRepositoryImpl
 import com.example.kmptemplate.repository.FeeCategoryRepository
 import com.example.kmptemplate.repository.FeeCategoryRepositoryImpl
+import com.example.kmptemplate.repository.ReceiptRepository
+import com.example.kmptemplate.repository.ReceiptRepositoryImpl
 import com.example.kmptemplate.repository.ReleaseSampleRepositoryImpl
 import com.example.kmptemplate.repository.SampleRepository
 import kotlinx.coroutines.Dispatchers
@@ -17,6 +26,8 @@ import org.koin.dsl.module
 
 val shareDiModule: Module =
     module {
+        val buildKonfigFlavor = BuildKonfigFlavor.getInstance()
+        val simulatedDataHolder = SimulatedDataHolder.makeTestData()
         single<SampleRepository> {
             if (BuildKonfigFlavor.getInstance() == BuildKonfigFlavor.RELEASE) {
                 ReleaseSampleRepositoryImpl()
@@ -25,11 +36,16 @@ val shareDiModule: Module =
             }
         }
         single<FeeCategoryDataSource> {
-            val dao = get<AppDatabase>().getFeeCategoryDao()
-            RoomFeeCategoryDataSource(dao)
+            RoomFeeCategoryDataSource(get())
         }
         single<FeeCategoryRepository> {
             FeeCategoryRepositoryImpl(get())
+        }
+        single<ReceiptDataSource> {
+            RoomReceiptDataSource(get(), get())
+        }
+        single<ReceiptRepository> {
+            ReceiptRepositoryImpl(get())
         }
         single<AppDatabase> {
             val builder = get<RoomDatabase.Builder<AppDatabase>>()
@@ -37,6 +53,20 @@ val shareDiModule: Module =
                 .setDriver(BundledSQLiteDriver())
                 .setQueryCoroutineContext(Dispatchers.IO)
                 .build()
+        }
+
+        // 環境によってData Access Objectを切り替える
+        single<FeeCategoryDao> {
+            when (buildKonfigFlavor) {
+                BuildKonfigFlavor.DEV -> SimulatedFeeCategoryDao(simulatedDataHolder)
+                BuildKonfigFlavor.RELEASE -> get<AppDatabase>().getFeeCategoryDao()
+            }
+        }
+        single<RoomReceiptDao> {
+            when (buildKonfigFlavor) {
+                BuildKonfigFlavor.DEV -> SimulatedRoomReceiptDao(simulatedDataHolder)
+                BuildKonfigFlavor.RELEASE -> get<AppDatabase>().getRoomReceiptDao()
+            }
         }
     }
 
