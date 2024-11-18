f495f7e51026bbdf3f22609e01f46f5651c8feeb
hagiharatomoaki <j8110074@gmail.com>
2024-11-18 23:36:59 +0900

    [Add] HeaderStateを追加

---
+++ androidApp/src/main/java/com/example/kmptemplate/android/uiState/HeaderState.kt
@@ -0,0 +1,7 @@
+package com.example.kmptemplate.android.uiState
+
+sealed interface HeaderState {
+    data object None: HeaderState
+    data class Normal(val msg: String): HeaderState
+    data class Error(val msg: String): HeaderState
+}
