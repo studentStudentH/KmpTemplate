42ac6aee3dac7999003b04063dd2b845fedc6eb9
hagiharatomoaki <j8110074@gmail.com>
2024-11-20 02:54:36 +0900

    [Update] SimulatedRoomReceiptDaoで通信遅延を表現

--- shared/src/commonMain/kotlin/com/example/kmptemplate/database/SimulatedRoomReceiptDao.kt
+++ shared/src/commonMain/kotlin/com/example/kmptemplate/database/SimulatedRoomReceiptDao.kt
@@ -1,17 +1,20 @@
 package com.example.kmptemplate.database
 
 import com.example.kmptemplate.domainmodel.FeeCategory
+import kotlinx.coroutines.delay
 
 internal class SimulatedRoomReceiptDao(
     private val dataHolder: SimulatedDataHolder,
 ) : RoomReceiptDao {
     override suspend fun loadById(receiptId: String): Map<RoomReceipt, FeeCategory> {
+        delay(DELAY_MILLIS)
         val receipt = dataHolder.receiptList.first { it.id == receiptId }
         val category = dataHolder.feeCategoryList.first { it.id == receipt.categoryId }
         return mapOf(receipt to category)
     }
 
     override suspend fun loadAll(): Map<RoomReceipt, FeeCategory> {
+        delay(DELAY_MILLIS)
         val receipts = dataHolder.receiptList
         val categories = dataHolder.feeCategoryList
         val map = mutableMapOf<RoomReceipt, FeeCategory>()
@@ -23,6 +26,7 @@ internal class SimulatedRoomReceiptDao(
     }
 
     override suspend fun insert(receipts: List<RoomReceipt>) {
+        delay(DELAY_MILLIS)
         dataHolder.receiptList += receipts
     }
 
@@ -30,6 +34,7 @@ internal class SimulatedRoomReceiptDao(
      * 計算量O(N^2)かかる実装なので大量のデータを扱うなら注意
      */
     override suspend fun update(receipts: List<RoomReceipt>) {
+        delay(DELAY_MILLIS)
         receipts.forEach {
             updateOne(it)
         }
@@ -48,8 +53,14 @@ internal class SimulatedRoomReceiptDao(
     }
 
     override suspend fun delete(receipts: List<RoomReceipt>) {
+        delay(DELAY_MILLIS)
         val receiptIds = receipts.map { it.id }
         val newReceipts = dataHolder.receiptList.filter { it.id !in receiptIds }
         dataHolder.receiptList = newReceipts
+    }
+
+    private companion object {
+        const val TAG = "SimulatedRoomReceiptDao"
+        const val DELAY_MILLIS = 3000L
     }
 }
