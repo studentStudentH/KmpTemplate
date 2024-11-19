6471cb8ae1faaa35c3055a4ee70ee8af7d582942
hagiharatomoaki <j8110074@gmail.com>
2024-11-20 02:42:22 +0900

    [Update] FullScreenModalの表示が崩れていた問題を修正

--- androidApp/src/main/java/com/example/kmptemplate/android/composable/FullScreenModal.kt
+++ androidApp/src/main/java/com/example/kmptemplate/android/composable/FullScreenModal.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -17,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.example.kmptemplate.android.MyApplicationTheme
 
 @Composable
@@ -26,6 +26,7 @@ fun FullScreenModal(
     content: @Composable () -> Unit,
 ) {
     Dialog(
+        properties = DialogProperties(usePlatformDefaultWidth = false),
         onDismissRequest = { onDismiss() }
     ) {
         Scaffold(
