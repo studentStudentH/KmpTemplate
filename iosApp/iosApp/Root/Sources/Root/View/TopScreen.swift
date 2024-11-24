35bcd88b05df0bfcdbc1492da04433786d5b366b
hagiharatomoaki <j8110074@gmail.com>
2024-11-24 23:14:02 +0900

    [Update] TopScreen.swiftにheaderを追加

--- iosApp/iosApp/Root/Sources/Root/View/TopScreen.swift
+++ iosApp/iosApp/Root/Sources/Root/View/TopScreen.swift
@@ -17,16 +17,19 @@ struct TopScreen: View {
             $0.makeCategorySummary()
         }
         NavigationView {
-            List {
-                Section(header: Text("統計")) {
-                    StatisticsPanel(categorySummaryList: categorySummaryList)
-                }
-                Section(header: Text("明細一覧")) {
-                    ForEach(receipts, id: \.id) { receipt in
-                        NavigationLink(
-                            destination: { Text("moved") },
-                            label: { ReceiptListItem(receipt: receipt) }
-                        )
+            VStack {
+                headerPanel()
+                List {
+                    Section(header: Text("統計")) {
+                        StatisticsPanel(categorySummaryList: categorySummaryList)
+                    }
+                    Section(header: Text("明細一覧")) {
+                        ForEach(receipts, id: \.id) { receipt in
+                            NavigationLink(
+                                destination: { Text("moved") },
+                                label: { ReceiptListItem(receipt: receipt) }
+                            )
+                        }
                     }
                 }
             }
@@ -37,6 +40,38 @@ struct TopScreen: View {
                 }
             }
         }
+    }
+
+    @ViewBuilder
+    func headerPanel() -> some View {
+        let headerState: HeaderState = .error(msg: "pseudo header msg")
+        // let headerState: HeaderState = .none
+        switch headerState {
+        case .none:
+            EmptyView()
+        case .normal(let msg):
+            makeHeaderPanel(labelText: msg, labelColor: .white, backgroundColor: .blue)
+        case .error(let msg):
+            makeHeaderPanel(labelText: msg, labelColor: .white, backgroundColor: .red)
+        }
+    }
+
+    @ViewBuilder
+    func makeHeaderPanel(
+        labelText: String,
+        labelColor: Color,
+        backgroundColor: Color
+    ) -> some View {
+        /// edgesIgnoringSafeAreaを設定しないとヘッダーの上まで色がついてしまう
+        HStack {
+            Text(labelText)
+                .font(.headline)
+                .foregroundStyle(labelColor)
+                .padding(.all, 4)
+            Spacer()
+        }
+        .frame(width: .infinity)
+        .background(backgroundColor.edgesIgnoringSafeArea(.horizontal))
     }
 }
 
