f8648a84323fd8f6754e385d5a67fc73900ba1e1
hagiharatomoaki <j8110074@gmail.com>
2024-11-24 22:33:04 +0900

    [Add] HeaderStateを追加

---
+++ iosApp/iosApp/Root/Sources/Root/UiState/HeaderState.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderState.swift
+//  Root
+//
+//  Created by hagihara tomoaki on 2024/11/24.
+//
+
+
+enum HeaderState {
+    case none
+
+    case normal(msg: String)
+
+    case error(msg: String)
+}
